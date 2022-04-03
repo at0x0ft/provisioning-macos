2d5ec5873cbbd459254526510996f22f6b5af299
at0x0ft <26642966+at0x0ft@users.noreply.github.com>
2022-04-04 00:15:02 +0900

    [WIP]: Add preference backup feature.

---
+++ preferences/restore.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env sh
+set -eu
+
+restore_backup_settings() {
+  # ref: https://github.com/ko1nksm/readlinkf/blob/master/readlinkf.sh
+  readlinkf() {
+    [ "${1:-}" ] || return 1
+    max_symlinks=40
+    CDPATH='' # to avoid changing to an unexpected directory
+
+    target=$1
+    [ -e "${target%/}" ] || target=${1%"${1##*[!/]}"} # trim trailing slashes
+    [ -d "${target:-/}" ] && target="$target/"
+
+    cd -P . 2>/dev/null || return 1
+    while [ "$max_symlinks" -ge 0 ] && max_symlinks=$((max_symlinks - 1)); do
+      if [ ! "$target" = "${target%/*}" ]; then
+        case $target in
+          /*) cd -P "${target%/*}/" 2>/dev/null || break ;;
+          *) cd -P "./${target%/*}" 2>/dev/null || break ;;
+        esac
+        target=${target##*/}
+      fi
+
+      if [ ! -L "$target" ]; then
+        target="${PWD%/}${target:+/}${target}"
+        printf '%s\n' "${target:-/}"
+        return 0
+      fi
+
+      # `ls -dl` format: "%s %u %s %s %u %s %s -> %s\n",
+      #   <file mode>, <number of links>, <owner name>, <group name>,
+      #   <size>, <date and time>, <pathname of link>, <contents of link>
+      # https://pubs.opengroup.org/onlinepubs/9699919799/utilities/ls.html
+      link=$(ls -dl -- "$target" 2>/dev/null) || break
+      target=${link#*" $target -> "}
+    done
+    return 1
+  }
+
+  local readonly SCRIPT_PATH=$(readlinkf "${0}")
+  local readonly SCRIPT_ROOT=$(basename -- "${SCRIPT_PATH}")
+  local readonly PREFERENCE_BACKUP_PATH="${SCRIPT_ROOT}/backup"
+
+  if [ -e "${PREFERENCE_BACKUP_PATH}" ]; then
+    # TODO: fix correct syntax
+    # defaults write <"${PREFERENCE_BACKUP_PATH}"
+    :
+  fi
+  return 0
+}
+
+restore_backup_settings
--- preferences/set.sh
+++ preferences/set.sh
@@ -1,6 +1,53 @@
 #!/usr/bin/env sh
 set -eu
 
+backup_old_settings() {
+  # ref: https://github.com/ko1nksm/readlinkf/blob/master/readlinkf.sh
+  readlinkf() {
+    [ "${1:-}" ] || return 1
+    max_symlinks=40
+    CDPATH='' # to avoid changing to an unexpected directory
+
+    target=$1
+    [ -e "${target%/}" ] || target=${1%"${1##*[!/]}"} # trim trailing slashes
+    [ -d "${target:-/}" ] && target="$target/"
+
+    cd -P . 2>/dev/null || return 1
+    while [ "$max_symlinks" -ge 0 ] && max_symlinks=$((max_symlinks - 1)); do
+      if [ ! "$target" = "${target%/*}" ]; then
+        case $target in
+          /*) cd -P "${target%/*}/" 2>/dev/null || break ;;
+          *) cd -P "./${target%/*}" 2>/dev/null || break ;;
+        esac
+        target=${target##*/}
+      fi
+
+      if [ ! -L "$target" ]; then
+        target="${PWD%/}${target:+/}${target}"
+        printf '%s\n' "${target:-/}"
+        return 0
+      fi
+
+      # `ls -dl` format: "%s %u %s %s %u %s %s -> %s\n",
+      #   <file mode>, <number of links>, <owner name>, <group name>,
+      #   <size>, <date and time>, <pathname of link>, <contents of link>
+      # https://pubs.opengroup.org/onlinepubs/9699919799/utilities/ls.html
+      link=$(ls -dl -- "$target" 2>/dev/null) || break
+      target=${link#*" $target -> "}
+    done
+    return 1
+  }
+
+  local readonly SCRIPT_PATH=$(readlinkf "${0}")
+  local readonly SCRIPT_ROOT=$(basename -- "${SCRIPT_PATH}")
+  local readonly PREFERENCE_BACKUP_PATH="${SCRIPT_ROOT}/backup"
+
+  if [ ! -e "${PREFERENCE_BACKUP_PATH}" ]; then
+    defaults read >"${PREFERENCE_BACKUP_PATH}"
+  fi
+  return 0
+}
+
 remap_capslock_to_command() {
   # TODO: implement later
   return 0
@@ -70,6 +117,7 @@ set_screenshot_fileext() {
   return 0
 }
 
+backup_old_settings
 remap_capslock_to_command
 set_shortcuts
 set_cursor_speed_fast
